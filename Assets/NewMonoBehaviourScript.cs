5645403aaa914eab2f267e1fe2424b2c15c04cf1
Hwang-Ji-Seon <lilelulallu@gmail.com>
2025-04-07 07:21:29 +0900

    Add files via upload

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello unity!");
+
+        int level = 5;
+        float strengh = 15.5f;
+        string playerName = "나검사";
+        bool isFullLevel=false;
+
+        Debug.Log("용사의 이름은?");
+        Debug.Log(playerName);
+        Debug.Log("용사의 레벨은?");
+        Debug.Log(level);
+        Debug.Log("용사의 힘은?");
+        Debug.Log(strengh);
+        Debug.Log("용사는 만렙인가?");
+        Debug.Log(isFullLevel);
+
+        //2.그룹형 변수
+        string[] monsters = { "슬라임", "사막뱀", "악마" };
+        int[] monsterLevel = new int[3];
+        monsterLevel[0]= 1;
+        monsterLevel[1] = 6;
+        monsterLevel[2] = 20;
+
+        Debug.Log("맵에 존재하는 몬스터의 레벨");
+        Debug.Log(monsterLevel[0]);
+        Debug.Log(monsterLevel[1]);
+        Debug.Log(monsterLevel[2]);
+
+        List<string> items= new List<string>();
+        items.Add("생명물약30");
+        items.Add("마나물약30");
+
+        //3.연산자
+        int exp = 1500;
+
+        exp = 1500 + 320;
+        exp = exp - 10;
+        level = exp / 300;
+        strengh = level * 3.1f;
+
+        Debug.Log("용사의 총 경험치는?");
+        Debug.Log(exp);
+        Debug.Log("용사의 레벨은?");
+        Debug.Log(level);
+        Debug.Log("용사의 힘은?");
+        Debug.Log(strengh);
+
+        int nextExp = 300 - (exp % 300);
+        Debug.Log("다음 레벨까지 남은 경험치는?");
+        Debug.Log(nextExp);
+
+        string title = "전설의";
+        Debug.Log("용사의 이름은?");
+        Debug.Log(title+" "+playerName);
+
+        int fullLevel = 99;
+        isFullLevel = level == fullLevel;
+        Debug.Log("용사는 만렙입니까?" + isFullLevel);
+
+        bool isEndTutorial = level > 10;
+        Debug.Log("튜토리얼이 끝난 용사입니까?" + isEndTutorial);
+
+        int health = 30;
+        int mana = 25;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
